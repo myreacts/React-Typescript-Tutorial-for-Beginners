e8556fbc797eb4e406ea79c0d51118c6275c5143
baizhi958216 <1475289190@qq.com>
2023-09-24 13:19:12 +0800

    feat: useState, setState
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import Heading from "./components/Heading"
-import { Section } from "./components/Section"
+import { useState } from "react";
+import Counter from "./components/Counter";
+import Heading from "./components/Heading";
+import { Section } from "./components/Section";
 
 function App() {
+  const [count, setCount] = useState<number>(1);
   return (
     <>
-      <Heading title={"Hello"}/>
+      <Heading title={"Hello"} />
       <Section>This is my Section.</Section>
+      <Counter setCount={setCount}>Count is {count}</Counter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
